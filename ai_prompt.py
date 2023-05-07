6817918fccbd608dc614d08fe272d40b110dce7f
Dechen Gao <gaodechen@live.cn>
2023-05-06 23:10:13 -0700

    keep system and user prompts

--- ai_prompt.py
+++ ai_prompt.py
@@ -8,7 +8,6 @@ class BasePrompt():
     
     def __init__(self, lang):
         self.lang = lang
-        
         if (self.lang not in BasePrompt.SUPPORT_LANGUAGES):
             raise NotImplementedError(
                 "The language {} is not supported.".format(self.lang))
@@ -58,32 +57,23 @@ class LyricPrompt(BasePrompt):
             "content": content[self.lang]
         }
 
-    def get_assistant_prompt(self):
-        ''' Requirements that the lyrics should meet '''''
-        content = {
-            "zh": "不要包含任何类似chorus或verse等音乐段落和结构名。\
-                    仅输出歌词正文，每句歌词独立成行。\
-                    在歌词开始之前输出[start]，歌词结束后输出[end]。\
-                    若无法生成歌词，请输出{error_message}。",
-            "en": "Do not mention or use any music structure names such as intro, chorus, verse.\
-                    Just write the lines of the lyrics with each line being separate and distinct.\
-                    Output [start] before the lyrics begin and [end] after the lyrics end.\
-                    If you cannot generate the lyrics, output {error_message}."
-        }
-        return {
-            "role": "assistant",
-            "content": content[self.lang].format(error_message=self.error_message)
-        }
-
     def get_user_prompt(self, user_demands):
         ''' Encode additional user demands for the lyrics '''
         content = {
-            "zh": "请生成歌词，这里是客户关于主题或风格的要求：{user_prompt}。",
-            "en": "Please write lyrics for a song. Here are additional requirements for the song you need to follow: {user_prompt}."
+            "zh": "请生成歌词，这里是客户关于主题或风格的要求：{user_prompt}。\
+                不要包含任何类似chorus或verse等音乐段落和结构名。\
+                仅输出歌词正文，每句歌词独立成行。\
+                在歌词开始之前输出[start]，歌词结束后输出[end]。\
+                若无法生成歌词，请输出{error_message}。",
+            "en": "Please write lyrics for a song. Here are additional requirements for the song you need to follow: {user_prompt}. \
+            Do not mention or use any music structure names such as intro, chorus, verse.\
+            Just write the lines of the lyrics with each line being separate and distinct.\
+            Output [start] before the lyrics begin and [end] after the lyrics end.\
+            If you cannot generate the lyrics, output {error_message}."
         }
         return {
             "role": "user",
-            "content": content[self.lang].format(user_prompt=user_demands)
+            "content": content[self.lang].format(user_prompt=user_demands, error_message=self.error_message)
         }
 
     def get_completion_prompt(self, user_demands):
@@ -91,7 +81,6 @@ class LyricPrompt(BasePrompt):
         return '\n'.join([
             prompt["content"] for prompt in [
                 self.get_system_prompt(),
-                self.get_assistant_prompt(),
                 self.get_user_prompt(user_demands),
             ] if "content" in prompt
         ])
@@ -100,6 +89,5 @@ class LyricPrompt(BasePrompt):
         ''' Prompts to the Chat API '''
         return [
             self.get_system_prompt(),
-            self.get_assistant_prompt(),
             self.get_user_prompt(user_demands),
         ]
